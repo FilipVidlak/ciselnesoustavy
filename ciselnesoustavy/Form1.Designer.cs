89a02ce329b74e2c270a026ca5487ba419afb0f4
Filip Vidlák <vidlakf.03@spst.eu>
2022-05-11 09:25:52 +0200

    Přidejte soubory projektu.

---
+++ ciselnesoustavy/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace ciselnesoustavy
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.maskedTextBox1.Location = new System.Drawing.Point(166, 112);
+            this.maskedTextBox1.Mask = "0000000000000000";
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(140, 24);
+            this.maskedTextBox1.TabIndex = 0;
+            this.maskedTextBox1.ValidatingType = typeof(int);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Menu;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.Location = new System.Drawing.Point(476, 66);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(186, 70);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Najdi prvočísla";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(134, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(206, 18);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Počet generovaných čísel:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Desktop;
+            this.ClientSize = new System.Drawing.Size(772, 199);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.maskedTextBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Hledač prvočísel";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ ciselnesoustavy/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ciselnesoustavy
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private bool JePrvocislo(int cislo)
+        {
+            if (cislo < 2) return false;
+            if (cislo == 2) return true;
+            for (int i = 3; i < Math.Sqrt(cislo); i += 2)
+            {
+                if (cislo % i == 0) return false;
+            }
+            return true;
+        }
+        Random rnd = new Random();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string prvocisla = " ";
+                int n = int.Parse(maskedTextBox1.Text);
+                int pocet = 0;
+                for (int i = 0; i < n; i++)
+                {
+                    int cislo = rnd.Next(5, 120);
+                    if (JePrvocislo(cislo))
+                    {
+                        pocet++;
+                        prvocisla += " ";
+                        prvocisla += cislo;
+                    }
+                }
+                if (pocet > 0) MessageBox.Show("Prvočísla jsou:" + prvocisla,"Nalezeno prvočíslo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                else MessageBox.Show("Žádné prvočíslo v posloupnosti není!", "Nenalezeno", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch
+            {
+                MessageBox.Show("Nezadal jsi číslo!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+ 
+}
